64083f92cd245976a08138814ea220ae2e9a6ff7
GalvinTiang <61193000+GalvinTiang@users.noreply.github.com>
2020-03-12 15:33:00 +0800

    Add files via upload

---
+++ Quiz1.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Quiz1 {
+      static boolean val1;
+      static double val2;
+      static float val3;
+      static int val4;
+      static long val5;
+      static String val6;
+      static char val7;
+      static byte val8;
+      static short val9;
+      public static void main (String [] args) {
+      
+      //Q1
+      String message;
+      
+      Scanner input = new Scanner(System.in);
+      
+      System.out.println("Enter a message:");
+      message = input.nextLine();
+      System.out.println(message);
+      
+      
+      //Q2
+  
+      
+      System.out.println("Default values for variables : ");
+      System.out.println("Variab1e 1 = " + val1);
+      System.out.println("Variab1e 2 = " + val2);
+      System.out.println("Variab1e 3 = " + val3);
+      System.out.println("Variab1e 4 = " + val4);
+      System.out.println("Variab1e 5 = " + val5);
+      System.out.println("Variab1e 6 = " + val6);
+      System.out.println("Variab1e 7 = " + val7);
+      System.out.println("Variab1e 8 = " + val8);
+      System.out.println("Variab1e 9 = " + val9);
+  
+      //Q3
+      /*String str1;
+      String str2;
+      
+      Scanner input = new Scanner(System.in);
+      
+      System.out.println("Enter string 1:");
+      str1 = input.nextLine();
+      System.out.println("Enter string 2;");
+      str2 = input.nextLine();
+      
+      if(str1.equals(str2))
+         System.out.println("String 1 is equal to String 2");
+         
+         else
+            System.out.println("String 1 is not equal to String 2");
+      */
+          }
+}    
+         
